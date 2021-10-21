a0c65b571ae48ab9f0f5b9ad7932ebd36178c207
= <=>
2021-10-21 10:12:16 +0800

    updates 0.4

--- app.js
+++ app.js
@@ -9,34 +9,91 @@ const app = Vue.createApp({
           parameters: [
             {
               id: 1,
-              name: "Parameter A: Student Services Program (SSP)",
-              title: "SYSTEM – INPUTS AND PROCESS",
+              name: "Parameter A: Statement of Vision, Mission, Goals and Objectives",
               content: [
+                { title: "SYSTEM – INPUTS AND PROCESS" },
                 {
-                  name: "S.1 – The institution has Student Services Program.",
+                  name: "S.1 – The institution has system of determining the Vision and Mission",
                   link: "",
                 },
                 {
-                  name: "S.2 – The institution has Student Services Program.",
+                  name: "S.2 – The vision clearly reflects what the institution hopes to become in the future",
+                  link: "",
+                },
+                {
+                  name: "S.3 – The vision clearly reflects what the institution's legal and other statutory mandates.",
+                  link: "",
+                },
+                {
+                  name: "S.4 – The Goals of the College/Academic Unit are consistent with the Mission of the Institution.",
+                  link: "",
+                },
+                {
+                  name: "S.5 – The Objectives of the program have the expected outcomes in terms of competencies (skills and knowledge), values and other attributes of the graduates which include the development of: ",
+                  link: "",
+                  subContent: [
+                    {
+                      name: "S.5.1 – technical/pedagogical skills;",
+                      link: "", 
+                    },
+                    {
+                      name: "S.5.2 – research and extension capabilities;",
+                      link: "", 
+                    },
+                    {
+                      name: "S.5.3 – student's own ideas, desirable attitudes and personal discipline;",
+                      link: "", 
+                    },
+                    {
+                      name: "S.5.4 – moral character;",
+                      link: "", 
+                    },
+                    {
+                      name: "S.5.5 – critical analytical, problem solving and other higher order thinking skills; and;",
+                      link: "", 
+                    },
+                    {
+                      name: "S.5.6 – aesthetic and cultural values.",
+                      link: "", 
+                    },
+                  ]
+                },
+                { title: "IMPLEMENTATION" },
+                {
+                  name: "I.1",
+                  link: "",
+                },
+                {
+                  name: "I.2",
+                  link: "",
+                },
+                {
+                  name: "I.3",
+                  link: "",
+                },
+                { title: "OUTCOME/S" },
+                {
+                  name: "O.1",
                   link: "",
                 },
               ],
             },
             {
-                id: 2,
-                name: "Parameter B: Student Services Program (SSP)",
-                title: "SYSTEM – INPUTS AND PROCESS",
-                content: [
-                  {
-                    name: "S.1 – Thess institution has Student Services Program.",
-                    link: "",
-                  },
-                  {
-                    name: "S.2 – Thess institution has Student Services Program.",
-                    link: "",
-                  },
-                ],
-              },
+              id: 2,
+              name: "Parameter B: Student Services Program (SSP)",
+              title: "SYSTEM – INPUTS AND PROCESS",
+              content: [
+                { title: "SYSTEM – INPUTS AND PROCESS" },
+                {
+                  name: "S.1 – Thess institution has Student Services Program.",
+                  link: "",
+                },
+                {
+                  name: "S.2 – Thess institution has Student Services Program.",
+                  link: "",
+                },
+              ],
+            },
           ],
         },
         {
@@ -108,6 +165,48 @@ const app = Vue.createApp({
       showDocuments: true,
       toggle: false,
       parameter_id: null,
+      areasByPPP: [
+        {
+          name: 'AREA I',
+          link: ''
+        },
+        {
+          name: 'AREA II',
+          link: ''
+        },
+        {
+          name: 'AREA III',
+          link: ''
+        },
+        {
+          name: 'AREA IV',
+          link: ''
+        },
+        {
+          name: 'AREA V',
+          link: ''
+        },
+        {
+          name: 'AREA VI',
+          link: ''
+        },
+        {
+          name: 'AREA VII',
+          link: ''
+        },
+        {
+          name: 'AREA VIII',
+          link: ''
+        },
+        {
+          name: 'AREA IX',
+          link: ''
+        },
+        {
+          name: 'AREA X',
+          link: ''
+        },
+      ],
     };
   },
   methods: {
@@ -127,9 +226,8 @@ const app = Vue.createApp({
       this.showDocuments = true;
     },
     showParameter(id) {
-        this.toggle = !this.toggle,
-        this.parameter_id = id
-    }
+      (this.toggle = !this.toggle), (this.parameter_id = id);
+    },
   },
 });
 
